4e9cdb7b848bf8207fb2c232b571ad47bf7a4f1c
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-09-15 09:31:09 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,27 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+        const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       {/* Do not remove the main div */}
+       <p>You've been on this page for {seconds} seconds.</p>
     </div>
-  )
+  );
+
+   
 }
 
 export default App
